5a5e42f5ea6c27c90519c5a479cb0bbb6e2599a7
liliwang <monsi0510@hotmail.com>
2020-10-22 14:29:25 -0500

    Add tests for all three methods

---
+++ src/test/java/CohortTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CohortTest {
+
+    Cohort cohort;
+    Student a;
+    Student b;
+
+    @BeforeEach
+    public void setUp() {
+        cohort = new Cohort();
+        a = new Student(1L, "amy");
+        b = new Student(2L, "Black");
+    }
+
+    @Test
+    public void testGetStudents() {
+        assertTrue(cohort.getStudents().isEmpty());
+    }
+
+    @Test
+    public void testAddStudent() {
+        assertTrue(cohort.getStudents().isEmpty());
+
+        cohort.addStudent(a);
+        assertFalse(cohort.getStudents().isEmpty());
+        assertEquals(1, cohort.getStudents().size());
+        assertSame(a, cohort.getStudents().get(0));
+
+        cohort.addStudent(b);
+        assertEquals(2, cohort.getStudents().size());
+        assertSame(b, cohort.getStudents().get(1));
+    }
+
+    @Test
+    public void testGetCohortAverage() {
+        a.addGrade(80);
+        a.addGrade(100);
+        b.addGrade(90);
+        b.addGrade(70);
+        cohort.addStudent(a);
+        cohort.addStudent(b);
+        assertEquals(85, cohort.getCohortAverage());
+        a.addGrade(88);
+        assertEquals(84.6, cohort.getCohortAverage(), 0.1);
+    }
+}
