94f3cfa952f0999d4a546874b1ff5dd79aa4d7be
liliwang <monsi0510@hotmail.com>
2020-10-22 10:21:58 -0500

    Add tests for CR functionality

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,11 +3,34 @@ import java.util.ArrayList;
 public class Student {
     public String name;
     public long id;
-    public ArrayList<Object> grades;
+    public ArrayList<Integer> grades;
 
     public Student(String name, long id) {
         this.name = name;
         this.id = id;
         this.grades = new ArrayList<>();
+    }
+
+    public long getID() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name = name;
+    }
+
+    public void addGrade(int i) {
+        grades.add(i);
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for (int grade : grades)
+            sum += grade;
+        return (double) sum / grades.size();
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -6,15 +7,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StudentTest {
 
+    private static Student a;
+    static ArrayList<Integer> list;
+
+    @BeforeAll
+    public static void setUp() {
+        a = new Student("lucy", 1);
+        list = new ArrayList<>();
+    }
+
     @Test
     public void testStudentConstructor() {
-        Student a = new Student("lucy", 1);
         assertNotNull(a);
         assertEquals("lucy", a.name);
         assertEquals(1, a.id);
 //        assertSame(new ArrayList<>(), a.grades);
-        assertEquals(new ArrayList<>(), a.grades);
+//        assertEquals(new ArrayList<>(), a.grades);
     }
 
+    @Test
+    public void testGetIdMethod() {
+        assertEquals(1, a.getID());
+    }
+
+    @Test
+    public void testGetNameMethod() {
+        assertEquals("lucy", a.getName());
+    }
+
+    @Test
+    public void testAddGradeMethod() {
+        list.add(60);
+        a.addGrade(60);
+        assertEquals(list, a.grades);
+    }
+
+    @Test
+    public void testGetGradesMethod() {
+        assertEquals(a.grades, a.getGrades());
+    }
+
+    @Test
+    public void testGetGradeAverage() {
+        a.addGrade(80);
+        a.addGrade(100);
+        list.add(80);
+        list.add(100);
+        assertEquals(list,a.getGrades());
+        assertEquals(list.size(),a.getGrades().size());
+        assertEquals(80.0, a.getGradeAverage());
+    }
 
 }
